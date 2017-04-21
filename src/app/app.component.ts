047f8fa1666d1b45b19ddef32bdc228c9b349aa8
ZachParris <zacharyjparris@gmail.com>
2017-04-20 20:22:50 -0500

    Added Model

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+import { Customer } from './model';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -8,12 +10,17 @@ import { Component } from '@angular/core';
 
 
 export class AppComponent  {
-  name = 'Bill Murray';
-  city = 'SomeTown';
-  street = '123 Belmont'
-  region = 'West';
-  hideAddress = false;
+  
+  customer: Customer = {
+    id: 1,
+    name: 'Bill Murray',
+    address: {
+      city: 'SomeTown',
+      street: '123 Belmont',
+      state: 'Texas',
+      region: 'West'
+    }
+  };
 
-  image = 'favicon.ico';
-  color = 'red';
+  hideAddress = false;
 }
