7a75712eb82314a1b515757dce40580f69d06b48
ZachParris <zacharyjparris@gmail.com>
2017-04-22 10:01:28 -0500

    added list binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,17 +10,53 @@ import { Customer } from './model';
 
 
 export class AppComponent  {
-  
-  customer: Customer = {
-    id: 1,
-    name: 'Bill Murray',
-    address: {
-      city: 'SomeTown',
-      street: '123 Belmont',
-      state: 'Texas',
-      region: 'West'
+  regions = ['West', 'East', 'North', 'South', 'Midwest'];
+
+  customers: Customer[] = [
+    {
+      id: 1,
+      name: 'Alex Smith',
+      address: {
+        street: '123 Main Street',
+        city: 'Anytown',
+        state: 'California',
+        region: 'West'
+      }
+    },
+    {
+      id: 2,
+      name: 'Pierre Pasmal',
+      address: {
+        street: '456 Rue de Main',
+        city: 'Quebec City',
+        state: 'Quebec',
+        region: 'East'
+      }
+    },
+    {
+      id: 3,
+      name: 'Margarita Nadie',
+      address: {
+        street: '789 Calle Principal',
+        city: 'Guadalajara',
+        state: 'Jalisco',
+        region: 'South'
+      }
+    },
+    {
+      id: 4,
+      name: 'Katie O\'Leary',
+      address: {
+        street: '137 DeKoven Street',
+        city: 'Chicago',
+        state: 'Illinois',
+        region: 'Midwest'
+      }
     }
-  };
+  ];
+
+  customer: Customer = this.customers[0];
+
 
   hideAddress = false;
 }
