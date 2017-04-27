90c2ce6a3b9ed50ae7db95d24c293c37e7c6c0e3
ZachParris <zacharyjparris@gmail.com>
2017-04-27 18:06:40 -0500

    added logger service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,8 @@ import { AppComponent } from './app.component';
 import {CustomerListComponent} from './customer-list.component';
 import {CustomerDetailComponent} from './customer-detail.component';
 import {AddressComponent} from './address.component';
+import { DataService } from './data.service';
+import { LoggerService } from './logger.service';
 
 @NgModule({
   declarations: [
@@ -20,7 +22,7 @@ import {AddressComponent} from './address.component';
     FormsModule,
     HttpModule
   ],
-  providers: [],
+  providers: [DataService, LoggerService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/customer-list.component.ts
+++ src/app/customer-list.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 import { Customer } from './model';
+import { DataService } from './data.service';
+import { LoggerService } from './logger.service';
 
 @Component({
   selector: 'customer-list',
@@ -9,52 +11,18 @@ import { Customer } from './model';
 })
 
 
-export class CustomerListComponent  {
-
-  customers: Customer[] = [
-    {
-      id: 1,
-      name: 'Alex Smith',
-      address: {
-        street: '123 Main Street',
-        city: 'Anytown',
-        state: 'California',
-        region: 'West'
-      }
-    },
-    {
-      id: 2,
-      name: 'Pierre Pasmal',
-      address: {
-        street: '456 Rue de Main',
-        city: 'Quebec City',
-        state: 'Quebec',
-        region: 'East'
-      }
-    },
-    {
-      id: 3,
-      name: 'Margarita Nadie',
-      address: {
-        street: '789 Calle Principal',
-        city: 'Guadalajara',
-        state: 'Jalisco',
-        region: 'South'
-      }
-    },
-    {
-      id: 4,
-      name: 'Katie O\'Leary',
-      address: {
-        street: '137 DeKoven Street',
-        city: 'Chicago',
-        state: 'Illinois',
-        region: 'Midwest'
-      }
-    }
-  ];
-
+export class CustomerListComponent implements OnInit  {
+  customers: Customer[] = []; 
   customer: Customer;
+
+  constructor(
+    private dataService: DataService, 
+    private loggerService: LoggerService) {};
+
+  ngOnInit(){
+    this.loggerService.log("Getting Customers...")
+    this.customers = this.dataService.getCustomers();
+  }
 
   shift(increment: number){
     let ix = this.customers.findIndex(c => c === this.customer) + increment;
