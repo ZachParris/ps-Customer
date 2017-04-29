6dbeae02150ea198775e3ee8c7cfba0dd37ba233
ZachParris <zacharyjparris@gmail.com>
2017-04-29 10:09:31 -0500

    Using Observables

--- src/app/customer-list.component.ts
+++ src/app/customer-list.component.ts
@@ -27,7 +27,8 @@ export class CustomerListComponent implements OnInit  {
   getCustomers() {
     this.isBusy = true;
     this.loggerService.log("Getting Customers...")
-    this.dataService.getCustomers().then(custs => {
+    //this.dataService.getCustomersP().then(custs => {
+    this.dataService.getCustomers().subscribe(custs => {
       this.customers = custs;
     });
   }
